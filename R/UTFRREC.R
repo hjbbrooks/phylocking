9d0f106684cc0a3004862d2fd85ba4e033e4c926
hjbbrooks <hbrooks9@vols.utk.edu>
2022-05-18 01:11:51 -0500

    where did it go

---
+++ R/UTFRREC.R
@@ -0,0 +1,26 @@
+#
+# A dataset containing the bird species common to the UT Forest Resources and AgResearch and Education Center (UTFRREC)
+#
+# @ format A data frame with 94 rows and 12 variables:
+# \describe{
+#   \item{Species}{scientific name}
+#   \item{Common}{common name}
+#   \item{Bander_abbr}{4 letter bander abbreviation}
+#   \item{Weight_oz}{average weight of the species (sexes combined), in ounces}
+#   \item{Length_in}{average length of the species (sexes combined), in inches}
+#   \item{MSF}{whether the species has been found in mixed species flocks}
+#   \item{Movement}{if the species is resident to UTFRREC or is a seasonal migrant}
+#   \item{Diet}{the primary dietary component for the species}
+#   \item{Habitat}{the primary habitat for the species}
+#   \item{Nesting}{the primary nesting location for the species}
+#   \item{Behavior}{the primary foraging behavior for the species}
+#   \item{Status}{the conservation status (low concern, declining, steep decline) of the species}
+# }
+#
+# @ source for species list \url{https://utarboretum.tennessee.edu/birds/}
+# @ source for species information \url{https://www.allaboutbirds.org/news/}
+# @ source for bander abbreviation \url{https://www.birdpop.org/docs/misc/Alpha_codes_eng.pdf}
+# @ source for tree (Kimball, R. T., Oliveros, C. H., Wang, N., White, N. D., Barker, F. K., Field, D. J., Ksepka, D. T., Chesser, R. T., Moyle, R. G., Braun, M. J., Brumfield, R. T., Faircloth, B. C., Smith, B. T., & Braun, E. L. (2019). A Phylogenomic Supertree of Birds. Diversity, 11(7), 109. https://doi.org/10.3390/d11070109)
+
+"UTFRREC"
+
